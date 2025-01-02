0366fbeb9939c7cf918835f42336e417bd4185fb
acetza <marquevjosman@gmail.com>
2025-01-01 21:50:49 -0600

    up

--- scenes/dudas/scripts/player.gd
+++ scenes/dudas/scripts/player.gd
@@ -1,25 +1,18 @@
 extends CharacterBody2D
 
+@onready var visuals: AnimatedSprite2D = $Visuals
 
-const SPEED = 200.0
-const JUMP_VELOCITY = -300.0
+const SPEED : float = 100.0
+const DELTA : float = 10.0
 
+func update_visuals(direction: Vector2) -> void:
+	if direction.length() == 0.0:
+		return
+	visuals.rotation = direction.angle()
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("move_left", "move_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	var direction : Vector2 = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	velocity.x = move_toward(velocity.x, SPEED * direction.x, DELTA)
+	velocity.y = move_toward(velocity.y, SPEED * direction.y, DELTA)
+	update_visuals(direction)
 	move_and_slide()
